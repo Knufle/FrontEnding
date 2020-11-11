3c5426df8076790a8be4aabeb78018a0c533f7ad
Marcel <marcelbastos12@hotmail.com>
2020-11-10 21:30:48 -0300

    setting favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,41 +2,62 @@ import { useState } from 'react';
 import Head from 'next/head';
 
 function IndexPage() {
-    return (
-      <div>
-        <Head>
-          <title>FrontEnding</title>
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="icon" />
-        </Head>
-        <Home/>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <Head>
+        <title>FrontEnding</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap"
+          rel="icon"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+      </Head>
+      <Home />
+    </div>
+  );
+}
 
 function Home() {
-    return (
-        <div>
-            <h1>Home</h1>
-            <Contador/>
-            <div>Teste</div>
-        </div>
-        )
+  return (
+    <div>
+      <h1>Home</h1>
+      <Contador />
+      <div>Teste</div>
+    </div>
+  );
 }
 
 function Contador() {
-    const [contador, setContador] = useState(1);
+  const [contador, setContador] = useState(1);
 
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
+  function adicionarContador() {
+    setContador(contador + 1);
+  }
 
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
+  return (
+    <div>
+      <div>{contador}</div>
+      <button onClick={adicionarContador}>Adicionar</button>
+    </div>
+  );
 }
 
-export default IndexPage
+export default IndexPage;
